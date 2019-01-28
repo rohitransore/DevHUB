544572190a9d871cdd4db51572b72a42f3cb650f
Rohit Ransore <rohitransore@rohits-macbook-pro.local>
2019-01-28 20:48:24 +0530

    Login Screen UI Done...

--- DevHUB/ViewController.swift
+++ DevHUB/ViewController.swift
@@ -8,11 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    @IBOutlet weak var EmailTextFieldOutlet: UITextField!
+    @IBOutlet weak var PasswordTextFieldOutlet: UITextField!
+    
+    
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //Getting Underline below Text Fields
+        configureTextField(x: 0, y: EmailTextFieldOutlet.frame.size.height-1.0, width: EmailTextFieldOutlet.frame.size.width, height:1.0, textField: EmailTextFieldOutlet)
+        configureTextField(x: 0, y: PasswordTextFieldOutlet.frame.size.height-1.0, width: PasswordTextFieldOutlet.frame.size.width, height:1.0, textField: PasswordTextFieldOutlet)
+    }
+    
+    //Configureing text field and putting underlines
+    func configureTextField(x:CGFloat,y:CGFloat,width:CGFloat,height:CGFloat,textField:UITextField)
+        
+    {
+        let bottomLine = CALayer()
+        bottomLine.frame = CGRect(x: x, y: y, width: width, height: height)
+        //bottomLine.backgroundColor = UIColor.black.cgColor
+        bottomLine.backgroundColor = UIColor.lightGray.cgColor
+        textField.borderStyle = UITextField.BorderStyle.none
+        textField.layer.addSublayer(bottomLine)
+        
+        
     }
 
 
